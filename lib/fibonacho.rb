f5839c2f725cac1935d8bcbb47e6df6b54c08b24
David A McClain <david@uhhuhyeah.com>
2011-09-24 15:23:05 -0700

    Constructing the loop and setting the method up to raise an error if something went wrong
    Signed-off-by: David A McClain <david@uhhuhyeah.com>

--- lib/fibonacho.rb
+++ lib/fibonacho.rb
@@ -1,3 +1,22 @@
 class Fixnum
-  def closest_fibonacci; end
+  def closest_fibonacci
+    # Basic idea:
+    # Generate a fibinacci sequence in a loop. At each iteration check to see if the last number created is greater than or equal to the input.
+    # If it is, break and return that last number. Otherwise keep the loop running
+    
+    
+    n = 0
+    sequence = []
+    while n < self
+      
+      n += 1
+    end
+    
+    if sequence.empty?
+      raise "Error: Fibinacci sequence could not be generated. Dagnabit!"
+    else
+      return sequence.last
+    end
+    
+  end
 end
