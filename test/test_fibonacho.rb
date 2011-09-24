a70c0a208a67b96dd7dfe4de3fa2db6c04c77971
David A McClain <david@uhhuhyeah.com>
2011-09-24 15:21:31 -0700

    Writing a testcase to check 'closest_fibonacci'
    Signed-off-by: David A McClain <david@uhhuhyeah.com>

--- test/test_fibonacho.rb
+++ test/test_fibonacho.rb
@@ -4,4 +4,8 @@ class TestFibonacho < Test::Unit::TestCase
   should "add the method 'closest_fibonacci' to Fixnum instances" do
      assert 5.respond_to?('closest_fibonacci'), 'Fixnum does not respond to is_a_fibonacci_number'
   end
+  
+  should "return the largest integer value from the Fibonacci Sequence smaller than the given integer" do
+    assert 156.closest_fibonacci == 144, '156.closest_fibonacci did not return 144 as desired'
+  end
 end
