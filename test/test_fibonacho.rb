64130ac328094d3088fd04b2ce1b4dee834ae56c
David A McClain <david@uhhuhyeah.com>
2011-09-24 15:41:31 -0700

    Adding some more test cases
    Signed-off-by: David A McClain <david@uhhuhyeah.com>

--- test/test_fibonacho.rb
+++ test/test_fibonacho.rb
@@ -6,6 +6,12 @@ class TestFibonacho < Test::Unit::TestCase
   end
   
   should "return the largest integer value from the Fibonacci Sequence smaller than the given integer" do
-    assert 156.closest_fibonacci == 144, '156.closest_fibonacci did not return 144 as desired'
+    assert 156.closest_fibonacci  == 144, '156.closest_fibonacci did not return 144 as desired'
+    assert 99.closest_fibonacci   == 89,  '99.closest_fibonacci did not return 89 as desired'
+    assert 62.closest_fibonacci   == 55,  '62.closest_fibonacci did not return 55 as desired'
+    assert 18.closest_fibonacci   == 13,  '18.closest_fibonacci did not return 13 as desired'
+    assert 2.closest_fibonacci    == 1,   '2.closest_fibonacci did not return 1 as desired'
+    assert 1.closest_fibonacci    == 0,   '1.closest_fibonacci did not return 0 as desired'
+    assert 0.closest_fibonacci    == 0,   '0.closest_fibonacci did not return 0 as desired'
   end
 end
