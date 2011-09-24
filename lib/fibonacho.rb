4b96fe5099cf47460641e0fb622591868438a6a3
David A McClain <david@uhhuhyeah.com>
2011-09-24 15:47:21 -0700

    Replacing array idea with two integer variables. Generating fibinacci sequence in loop and passing tests
    Signed-off-by: David A McClain <david@uhhuhyeah.com>

--- lib/fibonacho.rb
+++ lib/fibonacho.rb
@@ -4,19 +4,14 @@ class Fixnum
     # Generate a fibinacci sequence in a loop. At each iteration check to see if the last number created is greater than or equal to the input.
     # If it is, break and return that last number. Otherwise keep the loop running
     
-    
-    n = 0
-    sequence = []
-    while n < self
-      
-      n += 1
+    # Here a and b are standins for the current pair of numbers in the sequence. According to Fn = Fn-1 + Fn-2 the next number is the sum of the previous two numbers (a + b).
+    # While we're determining the next number, we assign b to a so that the current highest number in the seqence isn't lost
+    a = 0
+    b = 1
+    while b < self
+      a,b = b,a+b
     end
     
-    if sequence.empty?
-      raise "Error: Fibinacci sequence could not be generated. Dagnabit!"
-    else
-      return sequence.last
-    end
-    
+    return a    
   end
 end
